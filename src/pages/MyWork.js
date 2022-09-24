0b6c668f9538be902950e9d88d6879ae5fd97326
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-09-24 02:06:23 +0200

    add Work page

---
+++ src/pages/MyWork.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const MyWork = () => {
+    return (
+        <main id="my-work">
+            <div className="row">
+                <div className="container">
+                    <div className="row">
+                        <div className="rectangle"></div>
+                        <div className="rectangle"></div>
+                    </div>
+                    <div className="row">
+                        <div className="rectangle"></div>
+                        <div className="rectangle"></div>
+                    </div>
+                </div>
+                <div className="small-vertical"></div>
+                <div className="small-vertical"></div>
+            </div>
+
+            <div className="row">
+                <div className="square"></div>
+                <div className="square"></div>
+            </div>
+        </main>
+    )
+}
